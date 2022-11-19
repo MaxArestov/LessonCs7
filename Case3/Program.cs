0b480ecac178aacca920df93527b16a57c0956dc
Maxim Arestov <aresmax1993@gmail.com>
2022-11-19 10:59:40 +0300

    Complete all cases

--- Case3/Program.cs
+++ Case3/Program.cs
@@ -1,34 +1,42 @@
-﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
-//  Aₙₙ = m+n. Выведите полученный массив на экран.
-// m = 3, n = 4. 0 1 23
-// 1 2 34
-// 2 3 45
-
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 47 2
+// 5 92 3
+// 8 42 4
+//Сумма элементов главной диагонали: 1+9+2 = 12
+Console.Clear();
 Console.WriteLine("Введите m - ");
 bool checkM = int.TryParse(Console.ReadLine(), out  int m);
 Console.WriteLine("Введите n - ");
 bool checkN = int.TryParse(Console.ReadLine(), out  int n);
-
+int count = 0;
 if (!checkM || !checkN)
 {
     Console.WriteLine("Ошибка!");
     return;
 }
 
-int[,] array = CreateRandom2DArray(m,n);
+int[,] array = CreateRandom2DArray(m,n, out count);
 Print2DArray(array);
+Console.WriteLine($"Сумма всех чисел на главной диагонали - {count}");
 
-int[,] CreateRandom2DArray(int m, int n)
+int[,] CreateRandom2DArray(int m, int n, out int count)
 {
     int[,] array = new int[m, n];
-
+    count = 0;
     Random random  = new Random ();
 
     for (var i = 0; i < array.GetLength(0); i++)
     {
         for (var j = 0; j < array.GetLength(1); j++)
         {
-            array[i,j] = i + j;
+            array[i, j] = random.Next(0, 10);
+            if (i == j)
+            {
+                count += array[i, j];
+            }
         }
     }
 
